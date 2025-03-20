8ac5ed711e8be1187ecd04c8282af3b5f5cc6235
boyka <abdelrahmanzaki747@gmail.com>
2025-03-20 20:53:11 +0200

    setup server side

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
+
+const app = express();
+
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected To DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+
+// EndPoints
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(8000, () => {
+  connect();
+  console.log("Server Is Running!");
+});
---
+++ server/routes/auth.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {  } from '../controllers/auth.js';
+
+const router = express.Router();
+
+//Create a new user
+router.post("/signup", );
+//Sign in a user
+router.post("/signin",);
+
+//Google Auth
+router.post("/google");
+
+export default router;
